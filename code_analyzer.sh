96be51903ad422fee9a74c081f94525eecee950d
orezero <orezero.orezero@gmail.com>
2015-09-25 16:00:01 +0200

    Update code_analyzer.sh

--- code_analyzer.sh
+++ code_analyzer.sh
@@ -6,26 +6,30 @@ echo "
 ######################################################################################"
 echo "Source Code Analyzer for vulnerability assessment"
 rm -fr /var/www/risultati/
-mkdir /var/www/risultati
-touch /var/www/risultati/sqli_sql
-touch /var/www/risultati/sqli_db
-touch /var/www/risultati/sqli_dbuser
-touch /var/www/risultati/sqli_dbpassword
-touch /var/www/risultati/sqli_dbdatabase
-touch /var/www/risultati/sqli_dbhost
-touch /var/www/risultati/xss_echo
-touch /var/www/risultati/xss_get
-#####--Cerchiamo potenziali SQLi--#####
+mkdir /var/www/risultatii/sqli
+mkdir /var/www/risultati/xss
+touch /var/www/risultati/sqli/sqli_sql
+touch /var/www/risultati/sqli/sqli_db
+touch /var/www/risultati/sqli/sqli_dbuser
+touch /var/www/risultati/sqli/sqli_dbpassword
+touch /var/www/risultati/sqli/sqli_dbdatabase
+touch /var/www/risultati/sqli/sqli_dbhost
+touch /var/www/risultati/xss/xss_echo
+touch /var/www/risultati/xss/xss_get
+touch /var/www/risultati/xss/function_system
+touch /var/www/risultati/sqli/msql_query
+###--Cerchiamo potenziali SQLi--#####
 echo "Inserisci il  percorso dove effettuare la ricerca delle SQL Injection"
 read SQLi
-#####--Cerchiamo potenziali XSS--#####
 echo "Iserisci il percorso dove effettuare le ricerche degli XSS"
 read XSS
-grep -n -r '$sql_' $SQLi >> /var/www/risultati/sqli_sql
-grep -n -r '$db' $SQLi >> /var/www/risultati/sqli_db
-grep -n -r '$dbuser' $SQLi >> /var/www/risultati/sqli_dbuser
-grep -n -r '$dbpassword' $SQLi >> /var/www/risultati/sqli_dbpassword
-grep -n -r '$dbhost' $SQLi >> /var/www/risultati/sqli_dbdatabase
-grep -n -r '$dbdatabase' $SQLi >> /var/www/risultati/sqli_dbhost
-grep -n -r 'echo' $XSS >> /var/www/risultati/xss_echo
-grep -n -r '\$_GET' $XSS >> /var/www/risultati/xss_get 
+grep -n -r '$sql_' $SQLi >> /var/www/risultati/sqli/sqli_sql
+grep -n -r '$db' $SQLi >> /var/www/risultati/sqli/sqli_db
+grep -n -r '$dbuser' $SQLi >> /var/www/risultati/sqli/sqli_dbuser
+grep -n -r '$dbpassword' $SQLi >> /var/www/risultati/sqli/sqli_dbpassword
+grep -n -r '$dbhost' $SQLi >> /var/www/risultati/sqli/sqli_dbdatabase
+grep -n -r '$dbdatabase' $SQLi >> /var/www/risultati/sqli/sqli_dbhost
+grep -n -r 'system()' $SQLi >> /var/www/risultati/sqli/function_system
+grep -n -r 'mysql_query()' $SQLi >> /var/www/risultati/sqli/msql_query
+grep -n -r 'echo' $XSS >> /var/www/risultati/xss/xss_echo
+grep -n -r '\$_GET' $XSS >> /var/www/risultati/xss/xss_get
